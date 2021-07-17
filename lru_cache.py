77803b864a16c75d42536fde837d6dbb834268ce
NeerajS943 <70902457+NeerajS943@users.noreply.github.com>
2021-07-17 16:16:08 +0530

    assign2_commit2

--- lru_cache.py
+++ lru_cache.py
@@ -6,4 +6,11 @@ class LRU:
         self.cache = OrderedDict()
         self.capacity = capacity
     
-  
+    def get_cache(self):
+        pass
+    def get(self, key):
+        pass
+    def put(self, key, value):
+        pass
+    
+            
